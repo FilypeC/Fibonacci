f3c565c48fceb4bcab6c416417051709e4058150
FilypeC <filype.ramos@gmail.com>
2023-03-07 09:18:01 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Informe o numero que deseja consultar: ");
+
+        var consulta = int.Parse(Console.ReadLine());
+        int termo1 = 0;
+        int termo2 = 1;
+        int termo3 = 0;
+
+        while (termo3 <consulta) 
+        {
+            termo3 = termo1 + termo2;
+            termo1 = termo2;
+            termo2 = termo3;
+        }
+
+        if (consulta == termo3) {
+            Console.WriteLine("O valor pertence a sequencia de Fibonacci");
+        }
+        else Console.WriteLine("O valor não pertence a sequencia de Fibonacci");
+
+    }
+}
